7c6ab4134fce4bb72cfcbdb3cbb07ca5c24bb15d
William Casarin <jb55@jb55.com>
2023-12-16 16:24:55 -0800

    error: make it a bit more friendly for other libs

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     DbOpenFailed,
@@ -6,3 +8,18 @@ pub enum Error {
     NoteProcessFailed,
     TransactionFailed,
 }
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Error::DbOpenFailed => "Open failed",
+            Error::NotFound => "Not found",
+            Error::DecodeError => "Decode error",
+            Error::NoteProcessFailed => "Note process failed",
+            Error::TransactionFailed => "Transaction failed",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl std::error::Error for Error {}
